6e732be48403b57fc00357aec7a73793a3b66d32
ericksuzart <erick.suzart@ufba.br>
2023-07-01 01:50:12 -0300

    Add colors to logs

--- scripts/bmp2memory.py
+++ scripts/bmp2memory.py
@@ -2,6 +2,9 @@
 import intelhex
 import os
 import sys
+from colorama import init, Fore, Style
+
+init(autoreset=True)
 
 
 def read_bmp_as_hex(file_path):
@@ -59,52 +62,64 @@ def write_to_file(hex_list, file_path):
 
 
 def usage():
-    print("Usage: python bmp2memory.py <file_path> ... <word_size:int>")
+    print(f"{Style.BRIGHT}Usage: python bmp2memory.py <f1_path> <f2_path>" +
+          " ... <fn_path> <word_size:int>")
     print("Example: python bmp2memory.py image.bmp 6")
-    print("<file_path> is the path to the .bmp file")
+    print("<f_path> is the path to the .bmp file(s)")
     print("<word_size> is the number of bytes per line in the output hex file")
-    print("The output is the memory hex file")
-    print("that will be named as <file_path>.hex.")
-    print("OBS: the BMP file must be 360x360 pixels and 24 bits per pixel")
+    print("The output will be memory hex files with the same name as the" +
+          " input BMP files but with the .hex extension.")
+    print(f"{Style.BRIGHT}Note: The BMP file(s) must be 360x360 pixels and" +
+          " have 24 bits per pixel.")
 
 
 def process_bmp_file(bmp_file_path, word_size):
     if not os.path.exists(bmp_file_path):
-        raise FileNotFoundError(f"Error: File does not exist: {bmp_file_path}")
+        raise FileNotFoundError(
+            f"{Fore.RED}Error: File does not exist: {bmp_file_path}")
 
     if not bmp_file_path.endswith(".bmp"):
-        raise ValueError(f"Error: File is not a .bmp file: {bmp_file_path}")
+        raise ValueError(
+            f"{Fore.RED}Error: File is not a .bmp file: {bmp_file_path}")
 
-    print(f"Reading {bmp_file_path}")
+    print(f"{Fore.YELLOW}Reading {bmp_file_path}")
     hex_list = read_bmp_as_hex(bmp_file_path)
-    print(f"Converting from bottom->top to top->bottom from {bmp_file_path}")
+    print(f"{Fore.YELLOW}Converting from bottom->top to top->bottom" +
+          f" from {bmp_file_path}")
     hex_list = top_to_bottom_bmp(hex_list)
 
     hex_file_path = bmp_file_path + ".hex"
-    print(f"Writing the RGB bitmap to {hex_file_path}")
+    print(f"{Fore.YELLOW}Writing the RGB bitmap to {hex_file_path}")
     write_hex_list_to_hex_file(hex_list, hex_file_path, word_size)
-    print(f"Reading {hex_file_path} to crop it to the correct size")
+    print(f"{Fore.YELLOW}Reading {hex_file_path} to crop it to the" +
+          " correct size")
     hex_list = read_hex_file(hex_file_path)
-    print(f"Cropping {hex_file_path} to the correct size")
+    print(f"{Fore.YELLOW}Cropping {hex_file_path} to the correct size")
     hex_list = crop_hex_list_to_size(hex_list, 360 * 360 * 3 // word_size)
-    print(f"Writing the cropped {hex_file_path} to {hex_file_path}")
+    print(f"{Fore.YELLOW}Writing the cropped {hex_file_path} to" +
+          f" {hex_file_path}")
     write_to_file(hex_list, hex_file_path)
-    print(f"Done for {bmp_file_path}")
+    print(f"{Fore.GREEN}Done for {bmp_file_path}")
 
 
 def main():
+    if len(sys.argv) == 1 or sys.argv[1] == "-h" or sys.argv[1] == "--help":
+        usage()
+        return
+
     if len(sys.argv) < 3:
         usage()
-        raise ValueError("Error: Invalid number of arguments")
+        raise ValueError(f"{Fore.RED}Error: Invalid number of arguments")
 
     try:
         word_size = int(sys.argv[-1])
         if word_size <= 0:
             raise ValueError(
-                "Error: Word size must be an integer greater than 0")
+                f"{Fore.RED}Error: Word size must be greater than zero")
     except ValueError:
         usage()
-        raise ValueError("Error: Word size must be an integer greater than 0")
+        raise ValueError(
+            f"{Fore.RED}Error: Word size must be greater than zero")
 
     bmp_file_paths = sys.argv[1:-1]
 
