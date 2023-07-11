713dd8982031ce9e81b4617972925b8560df45c3
ericksuzart <erick.suzart@ufba.br>
2023-07-11 04:52:36 -0300

    Add IP modules, ram & PLL

--- quartus/Genius/ip_modules/green_on_rom.vhd
+++ quartus/Genius/ip_modules/green_on_rom.vhd
@@ -1,10 +1,10 @@
--- megafunction wizard: %RAM: 1-PORT%
+-- megafunction wizard: %ROM: 1-PORT%
 -- GENERATION: STANDARD
 -- VERSION: WM1.0
 -- MODULE: altsyncram 
 
 -- ============================================================
--- File Name: interface_ram.vhd
+-- File Name: green_on_rom.vhd
 -- Megafunction Name(s):
 -- 			altsyncram
 --
@@ -39,48 +39,43 @@ USE ieee.std_logic_1164.all;
 LIBRARY altera_mf;
 USE altera_mf.altera_mf_components.all;
 
-ENTITY interface_ram IS
+ENTITY green_on_rom IS
 	PORT
 	(
-		address		: IN STD_LOGIC_VECTOR (15 DOWNTO 0);
+		address		: IN STD_LOGIC_VECTOR (13 DOWNTO 0);
 		clock		: IN STD_LOGIC  := '1';
-		data		: IN STD_LOGIC_VECTOR (47 DOWNTO 0);
-		wren		: IN STD_LOGIC ;
-		q		: OUT STD_LOGIC_VECTOR (47 DOWNTO 0)
+		q		: OUT STD_LOGIC_VECTOR (15 DOWNTO 0)
 	);
-END interface_ram;
+END green_on_rom;
 
 
-ARCHITECTURE SYN OF interface_ram IS
+ARCHITECTURE SYN OF green_on_rom IS
 
-	SIGNAL sub_wire0	: STD_LOGIC_VECTOR (47 DOWNTO 0);
+	SIGNAL sub_wire0	: STD_LOGIC_VECTOR (15 DOWNTO 0);
 
 BEGIN
-	q    <= sub_wire0(47 DOWNTO 0);
+	q    <= sub_wire0(15 DOWNTO 0);
 
 	altsyncram_component : altsyncram
 	GENERIC MAP (
+		address_aclr_a => "NONE",
 		clock_enable_input_a => "BYPASS",
 		clock_enable_output_a => "BYPASS",
-		init_file => "../mem_init/sample_interface.bmp.hex",
+		init_file => "../mem_init/green_on.hex",
 		intended_device_family => "Cyclone IV E",
-		lpm_hint => "ENABLE_RUNTIME_MOD=YES,INSTANCE_NAME=IRAM",
+		lpm_hint => "ENABLE_RUNTIME_MOD=YES,INSTANCE_NAME=GREN",
 		lpm_type => "altsyncram",
-		numwords_a => 65536,
-		operation_mode => "SINGLE_PORT",
+		numwords_a => 14112,
+		operation_mode => "ROM",
 		outdata_aclr_a => "NONE",
 		outdata_reg_a => "UNREGISTERED",
-		power_up_uninitialized => "FALSE",
-		read_during_write_mode_port_a => "NEW_DATA_NO_NBE_READ",
-		widthad_a => 16,
-		width_a => 48,
+		widthad_a => 14,
+		width_a => 16,
 		width_byteena_a => 1
 	)
 	PORT MAP (
 		address_a => address,
 		clock0 => clock,
-		data_a => data,
-		wren_a => wren,
 		q_a => sub_wire0
 	);
 
@@ -94,7 +89,6 @@ END SYN;
 -- Retrieval info: PRIVATE: ADDRESSSTALL_A NUMERIC "0"
 -- Retrieval info: PRIVATE: AclrAddr NUMERIC "0"
 -- Retrieval info: PRIVATE: AclrByte NUMERIC "0"
--- Retrieval info: PRIVATE: AclrData NUMERIC "0"
 -- Retrieval info: PRIVATE: AclrOutput NUMERIC "0"
 -- Retrieval info: PRIVATE: BYTE_ENABLE NUMERIC "0"
 -- Retrieval info: PRIVATE: BYTE_SIZE NUMERIC "8"
@@ -102,57 +96,48 @@ END SYN;
 -- Retrieval info: PRIVATE: CLOCK_ENABLE_INPUT_A NUMERIC "0"
 -- Retrieval info: PRIVATE: CLOCK_ENABLE_OUTPUT_A NUMERIC "0"
 -- Retrieval info: PRIVATE: Clken NUMERIC "0"
--- Retrieval info: PRIVATE: DataBusSeparated NUMERIC "1"
 -- Retrieval info: PRIVATE: IMPLEMENT_IN_LES NUMERIC "0"
 -- Retrieval info: PRIVATE: INIT_FILE_LAYOUT STRING "PORT_A"
 -- Retrieval info: PRIVATE: INIT_TO_SIM_X NUMERIC "0"
 -- Retrieval info: PRIVATE: INTENDED_DEVICE_FAMILY STRING "Cyclone IV E"
 -- Retrieval info: PRIVATE: JTAG_ENABLED NUMERIC "1"
--- Retrieval info: PRIVATE: JTAG_ID STRING "IRAM"
+-- Retrieval info: PRIVATE: JTAG_ID STRING "GREN"
 -- Retrieval info: PRIVATE: MAXIMUM_DEPTH NUMERIC "0"
--- Retrieval info: PRIVATE: MIFfilename STRING "../mem_init/sample_interface.bmp.hex"
--- Retrieval info: PRIVATE: NUMWORDS_A NUMERIC "65536"
+-- Retrieval info: PRIVATE: MIFfilename STRING "../mem_init/green_on.hex"
+-- Retrieval info: PRIVATE: NUMWORDS_A NUMERIC "14112"
 -- Retrieval info: PRIVATE: RAM_BLOCK_TYPE NUMERIC "0"
--- Retrieval info: PRIVATE: READ_DURING_WRITE_MODE_PORT_A NUMERIC "3"
 -- Retrieval info: PRIVATE: RegAddr NUMERIC "1"
--- Retrieval info: PRIVATE: RegData NUMERIC "1"
 -- Retrieval info: PRIVATE: RegOutput NUMERIC "0"
 -- Retrieval info: PRIVATE: SYNTH_WRAPPER_GEN_POSTFIX STRING "0"
 -- Retrieval info: PRIVATE: SingleClock NUMERIC "1"
--- Retrieval info: PRIVATE: UseDQRAM NUMERIC "1"
--- Retrieval info: PRIVATE: WRCONTROL_ACLR_A NUMERIC "0"
--- Retrieval info: PRIVATE: WidthAddr NUMERIC "16"
--- Retrieval info: PRIVATE: WidthData NUMERIC "48"
+-- Retrieval info: PRIVATE: UseDQRAM NUMERIC "0"
+-- Retrieval info: PRIVATE: WidthAddr NUMERIC "14"
+-- Retrieval info: PRIVATE: WidthData NUMERIC "16"
 -- Retrieval info: PRIVATE: rden NUMERIC "0"
 -- Retrieval info: LIBRARY: altera_mf altera_mf.altera_mf_components.all
+-- Retrieval info: CONSTANT: ADDRESS_ACLR_A STRING "NONE"
 -- Retrieval info: CONSTANT: CLOCK_ENABLE_INPUT_A STRING "BYPASS"
 -- Retrieval info: CONSTANT: CLOCK_ENABLE_OUTPUT_A STRING "BYPASS"
--- Retrieval info: CONSTANT: INIT_FILE STRING "../mem_init/sample_interface.bmp.hex"
+-- Retrieval info: CONSTANT: INIT_FILE STRING "../mem_init/green_on.hex"
 -- Retrieval info: CONSTANT: INTENDED_DEVICE_FAMILY STRING "Cyclone IV E"
--- Retrieval info: CONSTANT: LPM_HINT STRING "ENABLE_RUNTIME_MOD=YES,INSTANCE_NAME=IRAM"
+-- Retrieval info: CONSTANT: LPM_HINT STRING "ENABLE_RUNTIME_MOD=YES,INSTANCE_NAME=GREN"
 -- Retrieval info: CONSTANT: LPM_TYPE STRING "altsyncram"
--- Retrieval info: CONSTANT: NUMWORDS_A NUMERIC "65536"
--- Retrieval info: CONSTANT: OPERATION_MODE STRING "SINGLE_PORT"
+-- Retrieval info: CONSTANT: NUMWORDS_A NUMERIC "14112"
+-- Retrieval info: CONSTANT: OPERATION_MODE STRING "ROM"
 -- Retrieval info: CONSTANT: OUTDATA_ACLR_A STRING "NONE"
 -- Retrieval info: CONSTANT: OUTDATA_REG_A STRING "UNREGISTERED"
--- Retrieval info: CONSTANT: POWER_UP_UNINITIALIZED STRING "FALSE"
--- Retrieval info: CONSTANT: READ_DURING_WRITE_MODE_PORT_A STRING "NEW_DATA_NO_NBE_READ"
--- Retrieval info: CONSTANT: WIDTHAD_A NUMERIC "16"
--- Retrieval info: CONSTANT: WIDTH_A NUMERIC "48"
+-- Retrieval info: CONSTANT: WIDTHAD_A NUMERIC "14"
+-- Retrieval info: CONSTANT: WIDTH_A NUMERIC "16"
 -- Retrieval info: CONSTANT: WIDTH_BYTEENA_A NUMERIC "1"
--- Retrieval info: USED_PORT: address 0 0 16 0 INPUT NODEFVAL "address[15..0]"
+-- Retrieval info: USED_PORT: address 0 0 14 0 INPUT NODEFVAL "address[13..0]"
 -- Retrieval info: USED_PORT: clock 0 0 0 0 INPUT VCC "clock"
--- Retrieval info: USED_PORT: data 0 0 48 0 INPUT NODEFVAL "data[47..0]"
--- Retrieval info: USED_PORT: q 0 0 48 0 OUTPUT NODEFVAL "q[47..0]"
--- Retrieval info: USED_PORT: wren 0 0 0 0 INPUT NODEFVAL "wren"
--- Retrieval info: CONNECT: @address_a 0 0 16 0 address 0 0 16 0
+-- Retrieval info: USED_PORT: q 0 0 16 0 OUTPUT NODEFVAL "q[15..0]"
+-- Retrieval info: CONNECT: @address_a 0 0 14 0 address 0 0 14 0
 -- Retrieval info: CONNECT: @clock0 0 0 0 0 clock 0 0 0 0
--- Retrieval info: CONNECT: @data_a 0 0 48 0 data 0 0 48 0
--- Retrieval info: CONNECT: @wren_a 0 0 0 0 wren 0 0 0 0
--- Retrieval info: CONNECT: q 0 0 48 0 @q_a 0 0 48 0
--- Retrieval info: GEN_FILE: TYPE_NORMAL interface_ram.vhd TRUE
--- Retrieval info: GEN_FILE: TYPE_NORMAL interface_ram.inc FALSE
--- Retrieval info: GEN_FILE: TYPE_NORMAL interface_ram.cmp TRUE
--- Retrieval info: GEN_FILE: TYPE_NORMAL interface_ram.bsf TRUE
--- Retrieval info: GEN_FILE: TYPE_NORMAL interface_ram_inst.vhd FALSE
+-- Retrieval info: CONNECT: q 0 0 16 0 @q_a 0 0 16 0
+-- Retrieval info: GEN_FILE: TYPE_NORMAL green_on_rom.vhd TRUE
+-- Retrieval info: GEN_FILE: TYPE_NORMAL green_on_rom.inc FALSE
+-- Retrieval info: GEN_FILE: TYPE_NORMAL green_on_rom.cmp TRUE
+-- Retrieval info: GEN_FILE: TYPE_NORMAL green_on_rom.bsf TRUE
+-- Retrieval info: GEN_FILE: TYPE_NORMAL green_on_rom_inst.vhd FALSE
 -- Retrieval info: LIB_FILE: altera_mf
